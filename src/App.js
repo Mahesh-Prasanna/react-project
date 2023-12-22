bba34c08382d3c29567ce563b62b8677e4ddc612
Mahesh Prasanna <damp.dassanayake@gmail.com>
2023-12-22 13:48:51 +0530

    Add useState to Project

--- src/App.js
+++ src/App.js
@@ -4,20 +4,51 @@ import Home from './pages/Home';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import About from './pages/About';
 import Contact from './pages/Contact';
+import { useState } from 'react';
 
 function App() {
+
+  const [count, setCount] = useState(0);
+
+  // let count = 0;
+
+  const increment = () =>{
+    // count +=1;
+    setCount(count+1);
+  };
+  const decrement = () =>{
+    // count -=1;
+    setCount(count-1);
+  };
+
+
+
   return (
-    <Router>
-      <Routes>
+    
 
-            <Route path='/' element={<Home/>}></Route>
+    <>
+      
+      <span className='title'>My Counter Application</span>
+      <h4 className='subTitle'>The Counnt is : {count}</h4>
+      <div className='actionButton'>
+        <button className='increaseButton' onClick={increment}>Increase</button>
+        <button className='decreaseButton' onClick={decrement}>Decrease</button>
+      </div>
 
-            <Route path='/about' element={<About/>}></Route>
+    </>
 
-            <Route path='/contact' element={<Contact/>}></Route>
 
-      </Routes>
-    </Router>
+    // <Router>
+    //   <Routes>
+
+    //         <Route path='/' element={<Home/>}></Route>
+
+    //         <Route path='/about' element={<About/>}></Route>
+
+    //         <Route path='/contact' element={<Contact/>}></Route>
+
+    //   </Routes>
+    // </Router>
   );
 }
 
