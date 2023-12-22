ba2c8cca35a9d1ff9f0be4240e895fd145c8baf0
Mahesh Prasanna <damp.dassanayake@gmail.com>
2023-12-22 12:29:46 +0530

    Add Display flex

--- src/components/TopicBox.js
+++ src/components/TopicBox.js
@@ -2,12 +2,17 @@ import React from 'react'
 import './TopicBox.css'
 
 export default function TopicBox() {
+    const topic = "My favourite food is Rice"
+    const item = " and Kottu "
+    const a = 10;
+    const b = 20;
+    const c = a + b;
   return (
     <div>
         <h1>This is my Topic Box</h1>
         <div className='topicBox'>
-            
-            <h2 className='text'>Topic Box</h2>
+            <h2 className='text'>{topic}{item}{c}</h2> <br />
+            <h2 className='text'>{[10, 20,39]}</h2>
         </div>
     </div>
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import TopicBox from '../components/TopicBox'
+import './Home.css'
 
 export default function Home() {
   return (
     <div>
         <h1>This is my Home page</h1>
         <TopicBox></TopicBox>
+        <div className='flex'>
+            <h2>First</h2>
+            <h2>Second</h2>
+            <h2>Third</h2>
+        </div>
+        
     </div>
+    
   )
 }
