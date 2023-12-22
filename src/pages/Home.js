c4a707bcc18ebb34ec1ea6f9961fb458b341f60e
Mahesh Prasanna <damp.dassanayake@gmail.com>
2023-12-22 12:44:52 +0530

    Add props and children props

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,7 +6,15 @@ export default function Home() {
   return (
     <div>
         <h1>This is my Home page</h1>
-        <TopicBox></TopicBox>
+        <TopicBox food='Kottu' price='20'>
+            <span>This is the description</span>
+        </TopicBox>
+        <TopicBox food='Rice' price='30'>
+            <button>This is a Button</button>
+        </TopicBox>
+        <TopicBox food='String Hopperts' price='50'>
+            <p>This is a paragraph</p>
+        </TopicBox>
         <div className='flex'>
             <h2>First</h2>
             <h2>Second</h2>
